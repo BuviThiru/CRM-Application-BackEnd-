53665e6c87b1f26c87c635ebacce1762de436898
Buvaneshwari <thirubuvana@gmail.com>
2023-05-09 10:34:56 +0530

    chors added

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const bodyParser = require('body-parser');
 const PORT = require("./config/server.config");
 const mongoDBURL = require('./config/db.config');
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({extended:true}));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 const authRoutes = require('./routes/auth.routes');
 authRoutes(app);
 const userRoutes = require('./routes/user.routes');
