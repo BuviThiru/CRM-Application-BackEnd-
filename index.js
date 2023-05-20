ad8a256c75a5f2b3a91fd9714f182baaa9ca2ce8
Buvaneshwari <thirubuvana@gmail.com>
2023-05-20 12:15:55 +0530

    cors 3

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const PORT = require("./config/server.config");
 const mongoDBURL = require('./config/db.config');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use((req, res, next) => {  
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,x-access-token');   
-    res.sendStatus(200);
-  });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
+  next();
+});
+
+
+
 const authRoutes = require('./routes/auth.routes');
 authRoutes(app);
 const userRoutes = require('./routes/user.routes');
