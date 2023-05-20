d760ceefd6f324622fb38015ffb2c99e12f2cfb7
Buvaneshwari <thirubuvana@gmail.com>
2023-05-20 12:06:35 +0530

    chors error 200 b

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const PORT = require("./config/server.config");
 const mongoDBURL = require('./config/db.config');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use((req, res, next) => {
+app.use((req, res, next) => {  
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,x-access-token');
-       next();
-       res.sendStatus(200);
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,x-access-token');   
+    res.sendStatus(200);
   });
 const authRoutes = require('./routes/auth.routes');
 authRoutes(app);
