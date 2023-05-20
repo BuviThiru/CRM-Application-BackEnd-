1230832d8953f0502fb600f559b9756d2bd2c6f2
Buvaneshwari <thirubuvana@gmail.com>
2023-05-20 12:00:38 +0530

    chors error 200 a

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type,x-access-token');
-    res.sendStatus(200);
-    next();
+       next();
+       res.sendStatus(200);
   });
 const authRoutes = require('./routes/auth.routes');
 authRoutes(app);
