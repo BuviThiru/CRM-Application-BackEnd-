c161d31d0ff01b752f94defd5ee3c8bfd1b91349
Buvaneshwari <thirubuvana@gmail.com>
2023-05-22 14:37:50 +0530

    chors for patch

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
   next();
 });
